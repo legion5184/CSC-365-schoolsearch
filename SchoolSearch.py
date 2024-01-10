8bf0486e5a0eb16d18bbd59d480272c88d3f73b3
legion5184 <96603312+legion5184@users.noreply.github.com>
2024-01-10 12:47:09 -0800

    Added Print feature and Getters to student

--- SchoolSearch.py
+++ SchoolSearch.py
@@ -1,13 +1,40 @@
 class Student:
 
-    def __init__(self, StLastName, StFirstName, Grade, Classroom, Bus, GPA, TLastName, TFirstName):
-        self.StLastName = StLastName
-        self.StFirstName = StFirstName
-        self.Grade = Grade
-        self.Classroom = Classroom
-        self.Bus = Bus
-        self.GPA = GPA
-        self.TLastName = TLastName
+    def __init__(self, Student):
+        self.StLastName = Student[0]
+        self.StFirstName = Student[1]
+        self.Grade = Student[2]
+        self.Classroom = Student[3]
+        self.Bus = Student[4]
+        self.GPA = float(Student[5])
+        self.TLastName = Student[6]
+        self.TFirstName = Student[7]
+
+    def getStLastName(self):
+        return self.StLastName
+
+    def getStFirstName(self):
+        return self.StFirstName
+
+    def getGrade(self):
+        return self.Grade
+    def getClassroom(self):
+        return self.Classroom
+
+    def getBus(self):
+        return self.Bus
+
+    def getGPA(self):
+        return self.GPA
+
+    def getTLastName(self):
+        return self.TLastName
+
+    def getTFirstName(self):
+        return self.TFirstName
+    def printStudent(self):
+        print(f"{self.StLastName},{self.StFirstName},{self.Grade},{self.Classroom},{self.Bus},{self.GPA},{self.TLastName},{self.TFirstName}")
+
 
 
 if __name__ == '__main__':
@@ -18,12 +45,12 @@ if __name__ == '__main__':
         content = file.readline()
         if not content:
             break
-        std_info = content.split();
+        std_info = content.split(",")
         Students.append(Student(std_info))
 
+        Students[0].printStudent()
+
     file.close()
-
-
 
 
 
