2cedc809e2e91d7175722a018f3f061e83cfc82e
legion5184 <96603312+legion5184@users.noreply.github.com>
2024-01-10 12:12:35 -0800

    Added Initial Python file

---
+++ SchoolSearch.py
@@ -0,0 +1,10 @@
+class Student:
+
+    def __init__(self, StLastName, StFirstName, Grade, Classroom, Bus, GPA, TLastName, TFirstName):
+        self.StLastName = StLastName
+        self.StFirstName = StFirstName
+        self.Grade = Grade
+        self.Classroom = Classroom
+        self.Bus = Bus
+        self.GPA = GPA
+        self.TLastName = TLastName
