6e6128df0b089c78acb45e3cc9d16ed007d6fa5c
Pablo Nguyen <pnguy150@calpoly.edu>
2024-01-10 15:48:12 -0800

    added user interaction: No error checking yet

--- SchoolSearch.py
+++ SchoolSearch.py
@@ -37,6 +37,8 @@ class Student:
 
 
 
+
+
 if __name__ == '__main__':
     Students = []
 
@@ -48,9 +50,52 @@ if __name__ == '__main__':
         std_info = content.split(",")
         Students.append(Student(std_info))
 
-        Students[0].printStudent()
-
     file.close()
+
+    while(True):
+        user_input = input("Input:\n").strip().split()
+
+
+        if user_input[0] == "Q" or user_input[0] == "Quit":
+            break
+
+        if user_input[0] == "I" or user_input[0] == "Info":
+            #Info Section Goes here
+            pass
+
+        if user_input[0] == "B" or user_input[0] == "Bus":
+            #Bus Action goes here
+            pass
+
+        if user_input[0] == "A" or user_input[0] == "Average":
+            #Average Action goes here
+            pass
+
+        if user_input[0] == "T" or user_input[0] == "Teacher":
+            #Teacher Action goes here
+            pass
+
+        if user_input[0] == "S" or user_input[0] == "Student":
+            try:
+                if user_input[2] == "B" or user_input[2] == "Bus":
+                    #put bus option for student here
+                    pass
+            except IndexError:
+                #put no bus option for student here:
+                pass
+
+        if user_input[0] == "G" or user_input[0] == "Grade":
+            try:
+                if user_input[2] == "H" or user_input[2] == "High":
+
+                    #put High option for Grade here
+                    pass
+                if user_input[2] == "L" or user_input[2] == "Low":
+                    #put low option for grade here
+                    pass
+            except IndexError:
+                #put no bound option for grade here
+                pass
 
 
 
